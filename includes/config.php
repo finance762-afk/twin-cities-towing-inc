c90032b4c89105c07dc2b15731b7496fba26333f
Calvin McKayle <calvin@pageone.cloud>
2026-07-22 19:20:58 +0000

    Add in-house spam shield to lead forms (honeypot+signed timestamp+JS signal)

--- includes/config.php
+++ includes/config.php
@@ -338,3 +338,4 @@ $trustSignals = [
     'Serving Richmond & Rosenberg TX',
     'Fast Response Times',
 ];
+$leadsFormSecret = 'bac7714a8f41505ab12d75311ccbb11a6374e38b1a010d69111c84a652cfa0f3'; // spam-shield HMAC (matches leads fn LEADS_FORM_SECRET)
